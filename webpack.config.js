de51f0c96b512a6570928a937c38209495dc6c78
vatsal <vatsal.pathak003@gmail.com>
2022-01-29 22:05:12 +0530

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/content_script/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/,
+      options: {
+        presets: [
+          ['@babel/preset-env', { targets: "defaults" }],
+          ['@babel/preset-react', {"runtime": "automatic"}]
+        ]
+      }
+    },
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    }
+  
+  ]
+  },
+  devtool:"eval-cheap-module-source-map"
+};
